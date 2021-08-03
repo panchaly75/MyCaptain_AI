f3fb6708a14b5fc398e29e2113cdba3e1f721870
Yash Panchal <72609549+panchaly75@users.noreply.github.com>
2021-08-03 19:10:21 +0530

    Let's Code !

---
+++ AI_MyCaptainApp_02.py
@@ -0,0 +1,5 @@
+#Displaying "Hello World" on the screen.
+#print is the Python defined default function, we are simply using it here print("Hello World!")
+print("Hello World!")
+
+#https://colab.research.google.com/drive/1Z4UYcGA8kFUmuxBDqudDd78FH4hkBk6H?usp=sharing
